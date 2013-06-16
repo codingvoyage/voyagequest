4a9ee6f8f614efe49ef5f93e8114ceaf381a8336
Brian Yang <brian@brianyang.me>
2013-06-16 12:28:24 -0400

    Visual Basic launcher for Windows

---
+++ VQLaunch.vb
@@ -0,0 +1,7 @@
+﻿Module VQLaunch
+    Sub Main()
+        ' Launch VoyageQuest
+        ' by Brian Yang
+        Shell("java -jar -Djava.library.path=""lib\lwjgl-2.9\native\windows"" VoyageQuest.jar", vbNormalFocus)
+    End Sub
+End Module
