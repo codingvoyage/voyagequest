6f39143728225a3340567fb03cefcb82e3c726c6
Edmund Qiu <edmundjqiu@gmail.com>
2013-06-18 02:11:37 -0400

    I wrote ScriptManager so that it explores the directory of the program looking for Script files. It then loads the Script files into a HashMap which maps the Script object to their name.

--- VoyageQuest/src/scripting/ScriptManager.java
+++ VoyageQuest/src/scripting/ScriptManager.java
@@ -1,14 +1,22 @@
 package scripting;
 
 
+import voyagequest.VoyageQuest;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
+import java.security.CodeSource;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+
+import java.io.File;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  *
@@ -17,6 +25,8 @@ import java.util.Scanner;
 public class ScriptManager {
     //An array of all the Script objects
     private Script[] scriptCollection;
+
+    private HashMap<String, Script> scriptHash;
     
     //A HashMap which acts as a dictionary which ties command names with
     //their corresponding integer IDs
@@ -31,6 +41,7 @@ public class ScriptManager {
     {
         //Let's make space for 20 scripts
         scriptCollection = new Script[SCRIPT_CAPACITY];
+        scriptHash = new HashMap<>();
         
         //Let's set each object to null
         for (int i = 0; i < SCRIPT_CAPACITY; i++) 
@@ -40,7 +51,8 @@ public class ScriptManager {
         
         //Now based on our definition file, load the IDDictionary
         commandIDDictionary = createIDDictionary("DICTIONARY.txt");
-        
+
+        loadAllScripts();
     }
     
     private HashMap<String, Integer> createIDDictionary(String dictionaryFilename) 
@@ -87,7 +99,140 @@ public class ScriptManager {
     {
         return commandIDDictionary;
     }
-    
+
+
+    /**
+     * Loads all the scripts by combing through the SCRIPT_FOLDER directory.
+     * Works for both programming in an IDE environment, as well as when the JAR file
+     * is executed because there is code for unzipping Jars, and code for exploring
+     * file directories normally. Someday, this code will likely be redone to make it
+     * more efficient/clean, but for now, this works.
+     */
+    public void loadAllScripts()
+    {
+        //The final list of Scripts, we need to find this by going through the SCRIPT_FOLDER
+        ArrayList<String> finalScriptPathList = new ArrayList<>();
+
+        //This is the location of our program, whether JAR or in the IDE
+        CodeSource src = getClass().getProtectionDomain().getCodeSource();
+        String programPath = src.getLocation().getPath();
+
+
+        try
+        {
+            //IF WE'RE RUNNING FROM A JAR FILE...
+            if (src.getLocation().getPath().endsWith(".jar") && src != null ) {
+
+                //Creates a ZipInputStream of this JAR file
+                URL jar = src.getLocation();
+                ZipInputStream zip = new ZipInputStream( jar.openStream());
+
+                //Goes through every file. This is probably not the most efficient. Sorry.
+                ZipEntry ze = null;
+                while( ( ze = zip.getNextEntry() ) != null ) {
+
+                    //Ignoring directories, see if any files are in the script directory
+                    if (!ze.isDirectory())
+                    {
+                        String entryName = ze.getName();
+                        if (entryName.contains(SCRIPT_FOLDER))
+                            finalScriptPathList.add(entryName.substring(SCRIPT_FOLDER.length()));
+                    }
+                }
+            }
+            else
+            {
+                //NOT RUNNING FROM JAR FILE - BUT FROM AN IDE ENVIRONMENT
+                //Use the exploreDirectory method I wrote to get all the files in the script folder
+                File scriptDirectory = new File(programPath + SCRIPT_FOLDER);
+                ArrayList<File> scriptFiles = this.exploreDirectory(scriptDirectory);
+
+                for (File file: scriptFiles)
+                {
+                    //You have something long
+                    //Something like C:\Users\Edmund\...VoyageQuest\scripting\scripts\SCENE1SCRIPTS\SCENE1
+                    //Trim so that only SCENE1SCRIPTS\SCENE1 remains
+                    String resultingPath = file.getPath().substring(scriptDirectory.getPath().length() + 1);
+
+                    //If the operating system decides to use \ symbols instead of / for whatever
+                    //reason, fix that.
+                    resultingPath.replace("\\", "/");
+                    finalScriptPathList.add(resultingPath);
+                }
+
+            }
+        }
+        catch (Exception e) {}
+
+        //At this point, all of the script paths should have been put in finalScriptPathList.
+        //Now, load them all.
+        for (String scriptPath: finalScriptPathList)
+            loadScript(scriptPath);
+
+
+    }
+
+
+    /**
+     * I got tired of looking for code to explore a directory, so I wrote my own which works
+     * recursively. It returns all the Files in the directory
+     * @param origin The path of the directory to explore
+     * @return an ArrayList of File objects within the directory. No subdirectories are included.
+     */
+    public ArrayList<File> exploreDirectory(File origin)
+    {
+        //Base case: does this directory no longer contain directories?
+        ArrayList<File> foundContents = new ArrayList<>();
+
+        File[] thisDirectoryContents = origin.listFiles();
+        for (File f : thisDirectoryContents)
+        {
+            //For each directory, expand recursively
+            if (f.isDirectory())
+                for (File eachItem : exploreDirectory(f)) foundContents.add(eachItem);
+            else
+                //If not a directory, just add the file
+                foundContents.add(f);
+        }
+
+        return foundContents;
+    }
+
+
+    public boolean loadScript(String filename)
+    {
+        int lastDashIndex = -1;
+        String scriptID = "";
+        //What we do depends on whether it's \\ or /
+        if (filename.contains("\\")  )
+        {
+            //The ID is the name of the script file.
+            lastDashIndex = filename.lastIndexOf("\\");
+
+        }
+        else if (filename.contains("/"))
+        {
+            lastDashIndex = filename.lastIndexOf("/");
+        }
+
+        //Take a substring starting from the dash index, plus one.
+        //If dash index was not found, then it should be -1 by default.
+        //But notice how -1 + 1 here is 0, so substring(0) returns itself anyway.
+        scriptID = filename.substring(lastDashIndex + 1);
+
+        //Debug info
+        System.out.println(filename + " has been loaded as " + scriptID);
+
+        //Return false if it already exists or the ID somehow turned out to be blank
+        if (scriptHash.containsKey(scriptID) || scriptID.equals("")) return false;
+
+        //From this point on, we're loading the script...
+        Script loadedScript = new Script(filename, commandIDDictionary);
+        scriptHash.put(scriptID, loadedScript);
+
+        return true;
+    }
+
     public boolean loadScript(String filename, int indexID) 
     {
         //We only want this function to work when there was no existing
@@ -109,6 +254,11 @@ public class ScriptManager {
     {
         //Refers to the array, find the one with scriptIDnumber
         return scriptCollection[scriptIDNumber];
+    }
+
+    public Script getScriptAtID(String scriptID)
+    {
+        return scriptHash.get(scriptID);
     }
     
 }
